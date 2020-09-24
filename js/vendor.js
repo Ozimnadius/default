a5ded03ee4ad28b5dec586afdf72b46662a029f0
Mikle Seregin <mikhail-seregin@yandex.ru>
2020-09-24 10:03:26 +0300

    first commit

---
+++ js/vendor.js
@@ -0,0 +1,3 @@
+// @prepros-append ../node_modules/jquery/dist/jquery.js
+// @prepros-append ../node_modules/swiper/js/swiper.js
+// @prepros-append ../node_modules/jquery.maskedinput/src/jquery.maskedinput.js
